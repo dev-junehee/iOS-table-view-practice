c7197e8d545c33848896c9dce2c77c2124d6c4bc
dev-junehee <dev.junehee@gmail.com>
2024-05-23 16:40:15 +0900

    Initial Commit

---
+++ iOS-table-view-practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-table-view-practice
+//
+//  Created by junehee on 5/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
